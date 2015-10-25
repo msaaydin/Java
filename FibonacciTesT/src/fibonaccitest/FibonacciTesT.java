dfbff6c15fc22f7715fa5ab5b4dab075012b462b
Musa <maydin@fsm.edu.tr>
2015-10-25 17:06:08 +0200

    new commit

---
+++ FibonacciTesT/src/fibonaccitest/FibonacciTesT.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonaccitest;
+
+/**
+ *
+ * @author MSa
+ */
+public class FibonacciTesT {
+
+    /**
+     * @param args the command line arguments
+     */
+     static long[]  NonRecursiveFibonancci(int term)
+    {
+        long [] fib = new long[term];                
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i < term; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib;
+    }
+     static long fibonacci(long n)
+    {
+            if (n == 0 || n == 1)  // base case
+               return n;
+            else
+                return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        long fbc[] = NonRecursiveFibonancci(100);
+        for (long a : fbc) {
+            System.out.print(a + " - ");        }
+        System.out.println("");
+        System.out.println("*****************************");
+        System.out.println("100 eleman için sonuç:"+fibonacci(100));
+    }
+    
+}
