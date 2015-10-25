1e3f6853321821a7dc84c9afffc097939705ddf6
Musa <maydin@fsm.edu.tr>
2015-10-25 21:05:42 +0200

    new commit

--- DoublyLinkedListNonGeneric/src/NonGeneric/Test.java
+++ DoublyLinkedListNonGeneric/src/NonGeneric/Test.java
@@ -9,7 +9,14 @@ public class Test {
         DLinkedList dlist = new DLinkedList();
 
         dlist.addFirst(3);
+        dlist.addFirst(25);
+        dlist.addLast(8785);
+        dlist.addLast(87);
+        dlist.addLast(5);
+        dlist.addLast(5454);        
         dlist.insertAfter(94, 0);
+        dlist.insertAfter(4, 2);
+        dlist.insertAfter(94888, 5);        
         dlist.printList();
     }
 }
