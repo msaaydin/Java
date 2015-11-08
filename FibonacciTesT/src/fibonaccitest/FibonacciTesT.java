7032f5dc1e1ae2058781b34a6ff541613be556b8
Musa <maydin@fsm.edu.tr>
2015-11-08 16:21:18 +0200

    new 8112015

--- FibonacciTesT/src/fibonaccitest/FibonacciTesT.java
+++ FibonacciTesT/src/fibonaccitest/FibonacciTesT.java
@@ -18,33 +18,35 @@ public class FibonacciTesT {
             long [] fib = new long[term];                
             fib[0] = 0;
             fib[1] = 1;
-            for (int i = 2; i < term; i++) {
+            for (int i = 2; i <term; i++) {
                 fib[i] = fib[i-1] + fib[i-2];
             }
             return fib;        
-        }
-       
+        }       
     }
      static long fibonacci(long n){
             if (n == 0 || n == 1)  // base case
                return n;
             else
                 return fibonacci(n-1) + fibonacci(n-2);
-    }
+    }     
      
-    public static void main(String[] args) {
-         
+    public static void main(String[] args) {         
         long start = System.nanoTime();
-        long fbc[] = NonRecursiveFibonancci(1);
+        long fbc[] = NonRecursiveFibonancci(10);
         long end = System.nanoTime();
-        long executiontime = end-start;
+        long executiontime = end - start;
         
         for (long a : fbc) {
             System.out.print(a + " - ");        
         }        
         System.out.println("");      
         System.out.println("program execution time.."+ executiontime +"..:nono seconds");
-        //System.out.println("100 eleman için sonuç:"+fibonacci(100));
-    }
-    
+        long start_recursive = System.nanoTime();
+        System.out.println("recursive  :"+fibonacci(10));
+        long end_recursive = System.nanoTime();
+        long executiontime_of_recursive = end_recursive - start_recursive;
+        
+        System.out.println("program execution time.."+ executiontime_of_recursive +"..:nono seconds");
+    }    
 }
