f29c5d4a1a5bc8e4a9778e371d8ec88d9ff5b905
Musa <maydin@fsm.edu.tr>
2015-12-09 13:48:13 +0200

    9122015

--- Recursive_test/src/recursive_test/Recursive_test.java
+++ Recursive_test/src/recursive_test/Recursive_test.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package recursive_test;
 
-/**
- *
- * @author rivendell
- */
+
 public class Recursive_test {
 
-    /**
-     * @param args the command line arguments
-     */
+   
      static int topla (int sayi){
      
         if (sayi < 1)
@@ -22,9 +13,49 @@ public class Recursive_test {
         
         
     }
+        public static void print (Node l)
+        {
+          if (l == null)
+            return;
+          else {
+            System.out.print(l.data+ " ->");
+            print(l.next);
+          }
+        }
+     public static int sumNodes(Node n, int sum){
+         if (n == null)
+             return 0;
+         else{
+             sum  = n.data;
+            // return n.data + sumNodes(n.next);
+            return sum + sumNodes(n.next, sum);
+         }
+     }
+     public static int length (Node n)
+     {
+        if (n == null)
+          return 0;
+        else
+          return 1 + length(n.next);
+     }
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println(topla(4));
+        Bliste lst1 = new Bliste(12);
+        lst1.addElementFront(45);
+        lst1.addElementFront(6523);        
+        lst1.addElementFront(623);
+        lst1.addElementFront(98);
+        lst1.addElementFront(4); 
+        print(lst1.head);
+        System.out.println("*******************");
+        int sum=0;
+        System.out.println("toplam non recursive...:"+lst1.sum());
+        System.out.println("toplam recursive "+sumNodes(lst1.head, sum));
+        System.out.println("size of list non recursive ..:"+lst1.count());
+        System.out.println("Size of list recursive......"+ length(lst1.head));
+        //lst1.printList();      
+
     }
     
 }
