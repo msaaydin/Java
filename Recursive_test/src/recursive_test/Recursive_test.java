6758d6fee30643d304214606a25db254144418bd
Musa <maydin@fsm.edu.tr>
2015-12-09 14:43:57 +0200

    9122015

--- Recursive_test/src/recursive_test/Recursive_test.java
+++ Recursive_test/src/recursive_test/Recursive_test.java
@@ -89,22 +89,23 @@ public class Recursive_test {
     public static void main(String[] args) {
         // TODO code application logic here
 //        System.out.println(topla(4));
-//        Bliste lst1 = new Bliste(12);
-//        lst1.addElementFront(45);
-//        lst1.addElementFront(6523);        
-//        lst1.addElementFront(623);
-//        lst1.addElementFront(98);
-//        lst1.addElementFront(4); 
-//        print(lst1.head);
-//        System.out.println("*******************");
-//        int sum=0;
-//        System.out.println("toplam non recursive...:"+lst1.sum());
+        Bliste lst1 = new Bliste(12);
+        lst1.addElementFront(45);
+        lst1.addElementFront(6523);        
+        lst1.addElementFront(623);
+        lst1.addElementFront(98);
+        lst1.addElementFront(4); 
+        print(lst1.head);
+        System.out.println("*******************");
+          int sum=0;
+          System.out.println("toplam non recursive...:"+lst1.sum());
 //        System.out.println("toplam recursive "+sumNodes(lst1.head, sum));
 //        System.out.println("size of list non recursive ..:"+lst1.count());
 //        System.out.println("Size of list recursive......"+ length(lst1.head));
 //        //lst1.printList();      
         System.out.println(e(3));
         System.out.println("count digit....:"+countDigit(1));
+        
     }
     
 }
