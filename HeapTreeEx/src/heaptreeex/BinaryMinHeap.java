98f0f189ede3f679471c58f583befe525b90b639
Musa <maydin@fsm.edu.tr>
2016-05-02 10:53:04 +0300

    812016

---
+++ HeapTreeEx/src/heaptreeex/BinaryMinHeap.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heaptreeex;
+
+/**
+ *
+ * @author rivendell
+ */
+public class BinaryMinHeap {
+
+    private int[] data;
+    private int heapSize;
+
+    public BinaryMinHeap(int size) {
+        data = new int[size];
+        heapSize = 0;
+    }
+
+    public int getMinimum() {
+        if (isEmpty()) {
+            throw new HeapException("Heap is empty");
+        } else {
+            return data[0];
+        }
+    }
+
+    public boolean isEmpty() {
+        return (heapSize == 0);
+    }
+
+    private int getLeftChildIndex(int nodeIndex) {
+        return 2 * nodeIndex + 1;
+    }
+
+    private int getRightChildIndex(int nodeIndex) {
+        return 2 * nodeIndex + 2;
+    }
+
+    private int getParentIndex(int nodeIndex) {
+        return (nodeIndex - 1) / 2;
+    }
+
+    public class HeapException extends RuntimeException {
+
+        public HeapException(String message) {
+            super(message);
+        }
+    }
+
+    public void insert(int value) {
+        if (heapSize == data.length) {
+            throw new HeapException("Heap's underlying storage is overflow");
+        } else {
+            heapSize++;
+            data[heapSize - 1] = value;
+            siftUp(heapSize - 1);
+        }
+    }
+
+    private void siftUp(int nodeIndex) {
+        int parentIndex, tmp;
+        if (nodeIndex != 0) {
+            parentIndex = getParentIndex(nodeIndex);
+            if (data[parentIndex] > data[nodeIndex]) {
+                tmp = data[parentIndex];
+                data[parentIndex] = data[nodeIndex];
+                data[nodeIndex] = tmp;
+                siftUp(parentIndex);
+            }
+        }
+    }
+    public void removeMin() {
+            if (isEmpty())
+                  throw new HeapException("Heap is empty");
+            else {
+                  data[0] = data[heapSize - 1];
+                  heapSize--;
+                  if (heapSize > 0)
+                        siftDown(0);
+            }
+      }
+    private void siftDown(int nodeIndex) {
+            int leftChildIndex, rightChildIndex, minIndex, tmp;
+            leftChildIndex = getLeftChildIndex(nodeIndex);
+            rightChildIndex = getRightChildIndex(nodeIndex);
+            if (rightChildIndex >= heapSize) {
+                  if (leftChildIndex >= heapSize)
+                        return;
+                  else
+                        minIndex = leftChildIndex;
+            } else {
+                  if (data[leftChildIndex] <= data[rightChildIndex])
+                        minIndex = leftChildIndex;
+                  else
+                        minIndex = rightChildIndex;
+            }
+            if (data[nodeIndex] > data[minIndex]) {
+                  tmp = data[minIndex];
+                  data[minIndex] = data[nodeIndex];
+                  data[nodeIndex] = tmp;
+                  siftDown(minIndex);
+            }
+      }
+
+}
---
+++ HeapTreeEx/src/heaptreeex/HeapTreeEx.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heaptreeex;
+
+/**
+ *
+ * @author rivendell
+ */
+public class HeapTreeEx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      BinaryMinHeap h = new BinaryMinHeap(10);
+      h.insert(234);
+      h.insert(65);
+      h.insert(24);
+      h.insert(59);
+      h.insert(5);
+      h.insert(2355554);
+      h.insert(2344);
+      
+      System.out.println(h.getMinimum());
+      
+    }
+    
+}
