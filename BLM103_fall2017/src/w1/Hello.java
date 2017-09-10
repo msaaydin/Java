5050aeea0f3bb04ad517c14daacd93829de93044
Erebor <Erebor@Gondor>
2017-09-10 16:07:37 +0300

    hello world commit

--- BLM103_fall2017/src/w1/Hello.java
+++ BLM103_fall2017/src/w1/Hello.java
@@ -11,6 +11,9 @@ package w1;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+       
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello");
+        }
     }
 }
