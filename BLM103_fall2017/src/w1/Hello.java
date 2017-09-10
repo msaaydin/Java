bf8bb672d910d8278813154aac2d6121a6551270
Erebor <Erebor@Gondor>
2017-09-10 16:06:09 +0300

    hello world commit

---
+++ BLM103_fall2017/src/w1/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1;
+
+/**
+ *
+ * @author Erebor
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
