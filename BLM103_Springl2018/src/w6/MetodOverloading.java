8647b07574d8306d442d3f38e913bc8c3c2f197d
Erebor <Erebor@Gondor>
2018-03-19 13:39:30 +0300

    passbyvalue

---
+++ BLM103_Springl2018/src/w6/MetodOverloading.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w6;
+
+/**
+ *
+ * @author Erebor
+ */
+public class MetodOverloading {
+    public static void artir(int y){
+        y++;
+        System.out.println("x in artir metodu içerisindeki değeri = "+y);
+    }
+     public static int artir2(int x){
+      x++;
+      System.out.println("x in artir metodu içerisindeki değeri = "+x);
+      return x;
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        System.out.println("x in main metod içerisindeki değeri ="+x);
+        artir(x);
+        artir(x);
+        x = 4;
+        artir(x+1);
+        artir(x);
+        System.out.println("x in metod çağrısından sonraki değeri="+x);
+        x = artir2(x);
+        x = artir2(x);
+        x = artir2(x);
+        x = artir2(x);
+        System.out.println("x in metod çağrısından sonraki değeri="+x);
+        
+        
+    }
+}
