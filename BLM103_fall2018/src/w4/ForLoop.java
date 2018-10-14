ea589b0fcfbc564c4307709a9782a1ab049038c3
Erebor <Erebor@Gondor>
2018-10-14 23:53:08 +0300

    forloop1

---
+++ BLM103_fall2018/src/w4/ForLoop.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w4;
+
+/**
+ *
+ * @author Erebor
+ */
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
